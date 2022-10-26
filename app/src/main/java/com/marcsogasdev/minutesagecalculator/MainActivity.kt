27a08732f3f3ac17bbbd61bbf84a611f02123fd7
MarcSogas <marc.sogas@gmail.com>
2022-10-26 21:01:55 +0200

    datapicker

--- app/src/main/java/com/marcsogasdev/minutesagecalculator/MainActivity.kt
+++ app/src/main/java/com/marcsogasdev/minutesagecalculator/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.marcsogasdev.minutesagecalculator
 
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
+import android.icu.util.Calendar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +16,34 @@ class MainActivity : AppCompatActivity() {
 
         val btnDataPicker:Button= findViewById(R.id.btnDatePicker)
 
+        btnDataPicker.setOnClickListener{ view ->
+            view
+             //data Picker
+            clickDatePicker()
+
+        }
+
+
+    }
+
+    @SuppressLint("NewApi")
+    fun clickDatePicker() {
+
+        //create claendar
+        val myCalendar=Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+
+        DatePickerDialog(this,
+        DatePickerDialog.OnDateSetListener{view,year, month, dayOfMonth ->
+
+        },
+            year,
+            month,
+            day
+
+        ).show()
 
     }
 }
