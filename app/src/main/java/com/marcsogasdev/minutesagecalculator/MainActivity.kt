216c230168af04e9275eff0d0254c523723db799
MarcSogas <marc.sogas@gmail.com>
2022-10-28 12:13:18 +0200

    selected date

--- app/src/main/java/com/marcsogasdev/minutesagecalculator/MainActivity.kt
+++ app/src/main/java/com/marcsogasdev/minutesagecalculator/MainActivity.kt
@@ -7,15 +7,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvSelectedDate:TextView?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
         val btnDataPicker:Button= findViewById(R.id.btnDatePicker)
+        tvSelectedDate= findViewById(R.id.tvSelectedDate)
 
         btnDataPicker.setOnClickListener{ view ->
             view
@@ -37,8 +42,12 @@ class MainActivity : AppCompatActivity() {
         val day = myCalendar.get(Calendar.DAY_OF_MONTH)
 
         DatePickerDialog(this,
-        {view,selectedYear, selectedMonth, selectedDay ->
-            Toast.makeText(this, "year: $selectedYear, month:${selectedMonth+1}, day: $selectedDay", Toast.LENGTH_LONG).show()
+        {view,selectedYear, selectedMonth, selectedDayOfMonth ->
+            Toast.makeText(this, "year: $selectedYear, month:${selectedMonth+1}, day: $selectedDayOfMonth", Toast.LENGTH_LONG).show()
+
+            val selectedDate="$selectedDayOfMonth/${selectedMonth+1}/$selectedYear"
+
+            tvSelectedDate?.text = selectedDate
 
         },
             year,
